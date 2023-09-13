22b702cac9719f566a90cdb76911a2eb9022a440
Sonam Angchuk <sangchuk@edpl.com>
2023-09-13 11:04:07 +0530

    initial files and project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/index.js", "./src/index.css"],
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'ert': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    extend: {},
+  },
+  plugins: [],
+}
+
